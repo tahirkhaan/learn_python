8bf62b04cb8b67cd13cdedc046d696d77d09787d
tahir <tahirkhan.7693@gmail.com>
2020-04-13 02:24:42 +0100

    learn basic knowledge of variables and functions

---
+++ variables.py
@@ -0,0 +1,31 @@
+character_name ="fahad"
+''' these are varibales here is a string'''
+character_age ="50"
+''' number variable'''
+character_gender ="male"
+is_male = False
+''' boolean value either its true or false'''
+
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old. ")
+print("he really liked the name " + character_name + " ,")
+print("but didn't like being " + character_age + ",")
+print("and he is " + character_gender + ",")
+'''
+print("coventry\nuniversity")
+phrase = "coventry university"
+print(phrase+ " is beautiful")
+'''
+'''How to use functions'''
+
+phrase = "TAHIR KHAN"
+print(phrase.lower().isupper())
+
+text = "fahad ali khan is a kind guy"
+print(len(text))
+
+
+my_num = 5
+'''print(10 % 3)'''
+print(my_num)
+print(4+5)
