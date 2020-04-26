168e5485b84348beff838d2a7b0276ad96d0ed20
tahir <tahirkhan.7693@gmail.com>
2020-04-26 22:19:02 +0100

    trying how to substract two numbers

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ num2 = input("Enter a another number:")
 result = int(num1) + int(num2)
 
 print(result)
-''' with this int itll will take the number 1 and 2  but its not adding the decimal numner '''
+''' with this int itll will take the number 1 and 2  but its not adding the decimal number '''
 
 
 num1 = input("Enter a first number:")
