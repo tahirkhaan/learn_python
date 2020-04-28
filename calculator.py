705bd6caccc6b086191ba96466ca0dfa542a061f
tahir <tahirkhan.7693@gmail.com>
2020-04-28 23:18:05 +0100

    trying to perform arithmetic operations in a different way

--- calculator.py
+++ calculator.py
@@ -1,14 +1,53 @@
-num1 = input("Enter a first number:")
-num2 = input("Enter a another number:")
+#Simple calculator
+# define my simple functions
 
-result = int(num1) + int(num2)
+def add(x, y):
+    return x + y
 
-print(result)
-''' with this int itll will take the number 1 and 2  but its not adding the decimal number '''
+def min(x, y):
+    return x - y
+
+def mul(x, y):
+    return x * y
+
+def div(x, y):
+    return x / y
+
+# provide options for operations
+print("Please select an operation.")
+print("For addition select 1. ")
+print("For subtraction select 2.")
+print("For multiplication  select 3.")
+print("For division select 4.")
+
+option = input("Please enter the number of your option 1, 2, 3, 4:")
+
+# limit option to 1,2,3,4
+if option > '4':
+    print("Invalid input. Please select 1,2,3 or 4.")
+
+elif option < '5':
+
+    print("Please select an operation.")
+    print("For addition select 1. ")
+    print("For subtraction select 2.")
+    print("For multiplication  select 3.")
+    print("For division select 4.")
 
 
-num1 = input("Enter a first number:")
-num2 = input("Enter a another number:")
+num1 = float(input( "Please enter the first value: "))
+num2 = float(input( "Please enter the second value: "))
 
-result = float(num1) + float(num2)
-print(result)
+
+
+if choice == '1':
+    print(num1, " + ", num2, "=", add(num1,num2))
+
+elif choice == '2':
+    print(num1, " - ", num2, "=", min(num1, num2))
+
+elif choice == '3':
+    print(num1, " * ", num2, "=", mul(num1,num2))
+
+elif choice =='4':
+    print(num1, " / ", num2, "=", div(num1, num2))
